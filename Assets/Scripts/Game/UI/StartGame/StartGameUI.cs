654376153946805217f28f721e604fd21e96da45
devAdaid <ahyeong2533@gmail.com>
2025-07-23 03:31:09 +0900

    UI 일부 지역화

--- Assets/Scripts/Game/UI/StartGame/StartGameUI.cs
+++ Assets/Scripts/Game/UI/StartGame/StartGameUI.cs
@@ -6,6 +6,11 @@ public class StartGameUI : UIBase
     [SerializeField]
     private Button startButton;
 
+    private void Awake()
+    {
+        this.SetActive(true);
+    }
+
     public override void Initialize()
     {
         startButton.onClick.AddListener(() => GameSystem.I.Event.InvokeEvent(new RequestStartGame()));
